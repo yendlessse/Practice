e4c559ed65fe047faef427a08fdd5be176459a5b
yendlessse <yoohanse@naver.com>
2025-09-05 16:16:49 +0900

    move: 파일 위치 변경

---
+++ 실습/practice1/level02/Application1.java
@@ -0,0 +1,9 @@
+package com.ohfiraffers.level02;
+
+public class Application1 {
+    public static void main(String[] args) {
+        char ch = 'a';
+
+        System.out.println("문자 a의 unicode :"+(int) ch);
+    }
+}
